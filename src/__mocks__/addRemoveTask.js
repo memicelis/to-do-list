0cd2fb4c0e3792fead10af353d87135a5e0b13a1
Elis Memic <elismemic5@gmail.com>
2023-08-26 11:58:37 +0200

    adjusted test data

--- src/__mocks__/addRemoveTask.js
+++ src/__mocks__/addRemoveTask.js
@@ -10,9 +10,9 @@ export const addTask = () => {
     },
     {
       description: 'Mock Test 2',
-      completed: false,
+      completed: true,
       index: 1,
-    },
+    }
   );
   localStorage.setItem('tasksData', JSON.stringify(listData));
   displayTasks();
